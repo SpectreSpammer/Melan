c7a3d350d006fb73a843ce8053948d391ce5cd72
meLagoo <melan.manalili@gmail.com>
2024-11-04 04:20:26 -0600

    Hi Sir,

--- src/main/java/day2_java_basics_syntax/MainJavaBasics.java
+++ src/main/java/day2_java_basics_syntax/MainJavaBasics.java
@@ -3,5 +3,6 @@ package day2_java_basics_syntax;
 public class MainJavaBasics {
     public static void main(String[] args) {
         System.out.println("Hello Melan");
+        System.out.println("Hi Sir!");
     }
 }
